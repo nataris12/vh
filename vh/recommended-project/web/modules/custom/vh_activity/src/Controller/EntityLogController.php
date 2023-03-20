50c9ee6de0caeab47d22efe759ec350fe15dedbb
Natalie Ris <nris@sgcmail.com>
2023-03-19 22:20:06 -0500

    vh activity first commit

---
+++ vh/recommended-project/web/modules/custom/vh_activity/src/Controller/EntityLogController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Drupal\vh_activity\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/*use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Routing\RedirectDestinationInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;*/
+
+/**
+ * VH Activity Log report
+ */
+class EntityLogController extends ControllerBase {
+
+  /**
+   * The Database Connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database')
+    );
+  }
+
+  /**
+   * TableSortExampleController constructor.
+   *
+   * @param \Drupal\Core\Database\Connection $database
+   *   A database connection object.
+   */
+  public function __construct(Connection $database) {
+    $this->database = $database;
+  }
+
+  /**
+   * Log page.
+   */
+  public function report() {
+
+    $header = [
+      ['data' => t('ID')],
+      ['data' => t('Timestamp')],
+      ['data' => t('Content Type')],
+      ['data' => t('Entity ID')],
+      ['data' => t('Event')],
+      ['data' => t('User ID')],
+    ];
+    
+    $query = $this->database->select('vh_activity', 'vh')->extend('Drupal\Core\Database\Query\PagerSelectExtender');
+    $query->fields('vh', ['id', 'timestamp', 'bundle', 'entity_id','event', 'uid']);
+    $query->orderBy('vh.id', 'ASC');
+    $result = $query->execute();
+
+    $rows = [];
+    foreach ($result as $row) {
+      
+      $rows[] = [
+        'data' => [
+          "id" => $row->id,
+          "timestamp" => date('F d, Y g:ia', $row->timestamp),
+          "bundle" => $row->bundle,
+          "entityid" => $row->entity_id,
+          "event" => $row->event,
+          "userid" => $row->uid,
+        ],
+
+      ];
+    }
+    
+    
+    $build['tablesort_table'] = [
+      '#theme' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    ];
+
+    // Attach the pager theme.
+    $build[] = ['#type' => 'pager'];
+
+    return $build;
+
+  }
+
+}
